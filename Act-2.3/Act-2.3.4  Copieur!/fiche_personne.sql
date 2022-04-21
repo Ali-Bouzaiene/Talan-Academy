cffb63debbdcd985c718563562f5bf3efaf2901d
Ali-Bouzaiene <103031816+Ali-Bouzaiene@users.noreply.github.com>
2022-04-21 08:32:33 +0100

    Add files via upload

---
+++ Act-2.3/Act-2.3.4  Copieur!/fiche_personne.sql
@@ -0,0 +1,35 @@
+create table fiche_personne (
+	id INT,
+	nom VARCHAR(50),
+	date_de_creation DATE
+);
+insert into fiche_personne (id, nom, date_de_creation) values (65, 'hcollihole0', '2013-06-11');
+insert into fiche_personne (id, nom, date_de_creation) values (79, 'bdemarchi1', '2016-12-25');
+insert into fiche_personne (id, nom, date_de_creation) values (26, 'ldring2', '2013-02-16');
+insert into fiche_personne (id, nom, date_de_creation) values (4, 'clebarre3', '2021-11-18');
+insert into fiche_personne (id, nom, date_de_creation) values (43, 'lbrader4', '2018-12-13');
+insert into fiche_personne (id, nom, date_de_creation) values (23, 'ccaldaro5', '2015-02-14');
+insert into fiche_personne (id, nom, date_de_creation) values (53, 'gcorinton6', '2018-09-21');
+insert into fiche_personne (id, nom, date_de_creation) values (8, 'jmcdade7', '2020-08-01');
+insert into fiche_personne (id, nom, date_de_creation) values (82, 'seisenberg8', '2014-11-21');
+insert into fiche_personne (id, nom, date_de_creation) values (62, 'ceustes9', '2013-03-08');
+insert into fiche_personne (id, nom, date_de_creation) values (47, 'demsliea', '2012-12-10');
+insert into fiche_personne (id, nom, date_de_creation) values (72, 'lfieldsb', '2016-02-24');
+insert into fiche_personne (id, nom, date_de_creation) values (100, 'fbottlec', '2018-11-12');
+insert into fiche_personne (id, nom, date_de_creation) values (12, 'blampend', '2012-04-19');
+insert into fiche_personne (id, nom, date_de_creation) values (8, 'sticke', '2016-06-02');
+insert into fiche_personne (id, nom, date_de_creation) values (75, 'dangelof', '2020-01-02');
+insert into fiche_personne (id, nom, date_de_creation) values (70, 'pladellg', '2021-06-21');
+insert into fiche_personne (id, nom, date_de_creation) values (62, 'mswyneh', '2019-02-14');
+insert into fiche_personne (id, nom, date_de_creation) values (85, 'pbelloni', '2017-03-07');
+insert into fiche_personne (id, nom, date_de_creation) values (82, 'fmarusicj', '2021-04-25');
+insert into fiche_personne (id, nom, date_de_creation) values (68, 'btripeank', '2015-04-17');
+insert into fiche_personne (id, nom, date_de_creation) values (33, 'acrimesl', '2016-04-25');
+insert into fiche_personne (id, nom, date_de_creation) values (93, 'vmcgivenm', '2017-02-23');
+insert into fiche_personne (id, nom, date_de_creation) values (26, 'mgrevesn', '2014-03-08');
+insert into fiche_personne (id, nom, date_de_creation) values (94, 'otrittamo', '2019-11-11');
+insert into fiche_personne (id, nom, date_de_creation) values (15, 'fturvillep', '2021-08-30');
+insert into fiche_personne (id, nom, date_de_creation) values (42, 'kkuhnelq', '2020-12-22');
+insert into fiche_personne (id, nom, date_de_creation) values (13, 'dspinleyr', '2020-08-29');
+insert into fiche_personne (id, nom, date_de_creation) values (9, 'hwintles', '2020-09-20');
+insert into fiche_personne (id, nom, date_de_creation) values (29, 'respyt', '2014-11-30');
